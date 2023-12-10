96ef05290890f94d6ac7dd83b087e15c9068a820
adeaashkreta <153109252+adeaashkreta@users.noreply.github.com>
2023-12-10 23:31:54 +0100

    Delete Blej directory

--- Blej/Jep.js
+++
@@ -1,7 +0,0 @@
-function submitForm() {
-    const itemName = document.getElementById('itemName').value;
-    const itemPrice = document.getElementById('itemPrice').value;
-    const itemDescription = document.getElementById('itemDescription').value;
-
-    alert(`Item Name: ${itemName}\nPrice: $${itemPrice}\nDescription: ${itemDescription}`);
-}
